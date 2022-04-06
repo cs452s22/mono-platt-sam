427b136845c109e8340c1cb0aef7096014eb4a81
Sam Platt <samplatt@sams-macbook.home>
2022-04-05 17:58:34 -0700

    lab2 commit

--- bagel/src/Bagel.elm
+++ bagel/src/Bagel.elm
@@ -1,2 +1,50 @@
-module Bagel exposing (double)
-double input = input * 2
+module Bagel exposing (main)
+
+import Browser
+import Html exposing (Html, button, div, input, pre, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
+
+-- Main
+
+main = 
+    Browser.sandbox { init = init, update = update, view = view }
+
+-- Model
+
+type alias Model =
+    { content : String
+    , inputContent : String
+    }
+
+init : Model
+init =
+    { content = ""
+    , inputContent = ""
+    }
+
+-- Update
+
+type Msg
+    = Scan
+    | Parse
+    | Change String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newInputContent ->
+            { model | inputContent = newInputContent }
+        Scan ->
+            { model | content = String.join " " (List.map (\str -> "<" ++ str ++ ">") (String.words model.inputContent)) }
+        Parse ->
+            { model | content = String.join "\n" (List.map (\str -> "<" ++ str ++ ">") (String.words model.inputContent)) }
+
+view : Model -> Html Msg
+view model = 
+    div []
+    [ input [ placeholder "Your text here", onInput Change ] []
+    , button [ onClick Scan ] [ text "Scan" ]
+    , button [ onClick Parse ] [ text "Parse" ]
+    , pre [] [ text ( model.content ) ]
+    ]
