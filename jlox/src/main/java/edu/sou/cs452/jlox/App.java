3b33ac4628d232083856be22644b5b849f823792
Sam Platt <samplatt@sams-macbook.home>
2022-04-15 23:29:24 -0700

    commenting out part 3 stuff so it will actually run

--- jlox/src/main/java/edu/sou/cs452/jlox/App.java
+++ jlox/src/main/java/edu/sou/cs452/jlox/App.java
@@ -5,15 +5,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 
-/**
+/*
  * Hello world!
- *
  */
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        // SpringApplication.run(App.class, args); // part 3 of lab 3???
+        System.out.println("Hello world");
     }
-
+    /* // part 3 of lab 3???
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -23,4 +23,5 @@ public class App {
             }
         };
     }
+    */
 }
