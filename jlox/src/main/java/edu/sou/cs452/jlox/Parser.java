41b24923701e587ac4dc5cc0d3aaf1a13a97542f
Sam Platt <samplatt@Sams-Macbook.local>
2022-04-17 20:51:40 -0700

    Parser stuff

--- jlox/src/main/java/edu/sou/cs452/jlox/Parser.java
+++ jlox/src/main/java/edu/sou/cs452/jlox/Parser.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import edu.sou.cs452.jlox.generated.types.*; // Token
 
+//TODO: Finish this stuff. Stopped after term() method
+
 public class Parser {
     private final List<Token> tokens;
     private int current = 0;
@@ -11,4 +13,79 @@ public class Parser {
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
+
+    private Expr expression() {
+        return equality();
+    }
+
+    private Expr equality() {
+        Expr expr = comparison();
+
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr comparison() {
+        Expr expr = term();
+
+        while (match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private Expr term() {
+        Expr expr = factor();
+
+        while (match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+
+        return expr;
+    }
+
+    private boolean match(TokenType... types) {
+        for (TokenType type : types) {
+            if (check(type)) {
+                advance();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean check(TokenType type) {
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance() {
+        if (!isAtEnd()) current++;
+        return previous();
+    }
+
+    private boolean isAtEnd() {
+        return peek().type == EOF;
+    }
+
+    private Token peek() {
+        return tokens.get(current);
+    }
+
+    private Token previous() {
+        return tokens.get(current - 1);
+    }
+
+
 }
