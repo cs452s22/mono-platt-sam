e8eae879c2a85ac404f87d7f09c837be7cca7615
Sam Platt <samplatt@Sams-Macbook.local>
2022-04-18 09:51:59 -0700

    added two missing lines

--- jlox/src/main/java/edu/sou/cs452/jlox/App.java
+++ jlox/src/main/java/edu/sou/cs452/jlox/App.java
@@ -1,13 +1,15 @@
 package edu.sou.cs452.jlox;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
 
 /*
  * Hello world!
  */
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args); // part 3 of lab 3???
