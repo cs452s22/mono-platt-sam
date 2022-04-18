08bc96761ee5289166e2191eb5f75e15fc1a1228
Sam Platt <samplatt@Sams-Macbook.local>
2022-04-18 13:47:37 -0700

    renaming files is annoying when git gets involved

---
+++ jlox/src/main/java/edu/sou/cs452/jlox/TokensDatafetcher.java
@@ -0,0 +1,16 @@
+package edu.sou.cs452.jlox;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+import edu.sou.cs452.jlox.generated.types.*;
+import java.util.List;
+
+@DgsComponent
+public class TokensDatafetcher {
+    @DgsQuery
+    public List<Token> tokens(@InputArgument String code) { // changed from titleFilter to code
+        Scanner sc = new Scanner(code); // changed from titleFilter to code
+        return sc.scanTokens(); // call new scanner and return the scanned tokens to be output to DGS in graphql
+    }
+}
